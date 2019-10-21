d4a3edf5b424deccfef54c2b23f59f7a126efeb2
GiuliBentancor <54630519+GiuliBentancor@users.noreply.github.com>
2019-10-21 19:47:48 -0300

    Add files via upload

--- ecommerce-master/js/cart.js
+++ ecommerce-master/js/cart.js
@@ -1,37 +1,74 @@
-let productUnitCost = 0;
-let productCurrency = "";
-let subtotal = 0;
-let shippingPercentage = 0.15;
-let total = 0;
-let paymentTypeSelected = false;
-const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
-const BANKING_PAYMENT = "Transferencia bancaria";
-let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
+let unitCost= 0;
+//let productCurrency = "";
+//let subtotal = 0;
+//let shippingPercentage = 0.15;
+//let total = 0;
+//let paymentTypeSelected = false;
+//const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
+//const BANKING_PAYMENT = "Transferencia bancaria";
+//let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
+
 
 //Función que se utiliza para actualizar los costos de publicación
-function updateTotalCosts(){
+//function updateTotalCosts(){}
 
-}
 
 function updateSubtotal(){
+  document.getElementById("subtotal").innerHTML= 
+  parseInt(document.getElementById("count").value) * unitCost ;
+  
+  document.getElementById("subtotal2").innerHTML= 
+  parseInt(document.getElementById("count").value) * unitCost ;
 
 }
 
-function showPaymentTypeNotSelected(){
+//function showPaymentTypeNotSelected(){}
 
+//function hidePaymentTypeNotSelected(){}
+
+
+function showArticles(array){
+
+    let contenido = "";
+
+    for(let i = 0; i < array.length; i++){
+        let producto = array[i];
+
+        unitCost= producto.unitCost
+        
+        contenido += `
+             <td id="imagen"> <img src=" `+ producto.src +`" width="80%" > </td>          
+             <td id="name"> `+ producto.name +` </td>
+             <td id="unitCost">` + producto.unitCost + `</td>
+             <td id="currency">` + producto.currency + `</td>
+             <td id="cantidad"> <input type="number" id="count" placeholder="" required="" value="1" min="0" onchange='updateSubtotal();'> </td>
+             <td id="subtotal">  </td>
+    
+    `
+
+
+    }
+    
+    document.getElementById("art-info").innerHTML = contenido;
+    updateSubtotal();
 }
+         
 
-function hidePaymentTypeNotSelected(){
-
-}
-
-function showArticles(articles){
-
-}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+ 
 
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+
+           showArticles(resultObj.data.articles);
+        }
+         else {
+            alert ('no funciono');
+        }
+    });
 });
+
