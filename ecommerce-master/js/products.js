431c00684b062663c7beed7eb3e7bef7685969e2
GiuliBentancor <54630519+GiuliBentancor@users.noreply.github.com>
2019-11-14 16:14:17 -0300

    Add files via upload

--- ecommerce-master/js/products.js
+++ ecommerce-master/js/products.js
@@ -35,20 +35,19 @@ function showProductsList(){
 
 
             htmlContentToAppend += `
-            <a href="product-info.html" class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ products.name +`</h4>
+            
+            <div class="col-md-4">
+            <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class='bd-placeholder-img card-img-top'>
+                    <div class="card-body">
+                        
+                            <h3 class="m-3">`+ products.name +`</h3>
                             <small class="text-muted">` + products.cost + ` ` +  products.currency + ` </small>
-                        </div>
-                        <p class="mb-1">` + products.description + `</p>
-                    </div>
+                        <p class="card-text">` + products.description + `</p>          
                 </div>
+                
             </a>
+            </div>
             `
         }
 
