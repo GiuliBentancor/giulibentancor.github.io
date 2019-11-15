02cdcdea1be98d77b63bfaa841e110154ba98461
GiuliBentancor <54630519+GiuliBentancor@users.noreply.github.com>
2019-11-15 10:26:50 -0300

    Update cart.js

--- ecommerce-master/js/cart.js
+++ ecommerce-master/js/cart.js
@@ -18,7 +18,7 @@ function updateTotalCosts(){
     let comissionCostHTML = document.getElementById("comissionEnvio");
     let totalCostHTML = document.getElementById("totalCostText");
 
-    let comissionToShow = Math.round(subtotal * shippingPercentage);
+    let comissionToShow = (Math.round(subtotal * shippingPercentage));
     let totalCostToShow = (Math.round(subtotal * (1 + shippingPercentage)));
 
     comissionCostHTML.innerHTML = comissionToShow;
